b7083cd94b17aa714c66f49186f3ce14128376b3
Francisco Galeano <fmagaleano@gmail.com>
2024-10-10 01:29:55 -0300

    diseño-carga-50codes

--- carga-codigos.js
+++ carga-codigos.js
@@ -13,11 +13,57 @@ const codeSchema = new mongoose.Schema({
 
 const Code = mongoose.model('Code', codeSchema);
 
-const codesToAdd = [
-    { code: 'CODIGO123' },
-    { code: 'CODIGO456' },
-    { code: 'CODIGO789' }
-];
+    const codesToAdd = [
+        { code: 'AJV237' },
+        { code: 'OQY556' },
+        { code: 'CVN120' },
+        { code: 'RUW304' },
+        { code: 'TPZ475' },
+        { code: 'LZK681' },
+        { code: 'FBR519' },
+        { code: 'GEU861' },
+        { code: 'ZWX438' },
+        { code: 'JID932' },
+        { code: 'WPS263' },
+        { code: 'EQS593' },
+        { code: 'UBM826' },
+        { code: 'NQA012' },
+        { code: 'PHR603' },
+        { code: 'YZK374' },
+        { code: 'MIJ759' },
+        { code: 'DUK032' },
+        { code: 'SOV117' },
+        { code: 'XGR949' },
+        { code: 'CZV580' },
+        { code: 'TAQ201' },
+        { code: 'JDW689' },
+        { code: 'KHW204' },
+        { code: 'SRB742' },
+        { code: 'FGP319' },
+        { code: 'EZI230' },
+        { code: 'WJA644' },
+        { code: 'BXU157' },
+        { code: 'MFH692' },
+        { code: 'VCB488' },
+        { code: 'RDP307' },
+        { code: 'YUH583' },
+        { code: 'GLF024' },
+        { code: 'QLO168' },
+        { code: 'JRV742' },
+        { code: 'WZH812' },
+        { code: 'MUL013' },
+        { code: 'VTB204' },
+        { code: 'PCE763' },
+        { code: 'USG430' },
+        { code: 'ZWB123' },
+        { code: 'FYD506' },
+        { code: 'LMA934' },
+        { code: 'CPT478' },
+        { code: 'RQJ209' },
+        { code: 'HRV671' },
+        { code: 'GYW982' },
+        { code: 'WQO725' }
+    ];
 
 const addCodes = async () => {
     try {
